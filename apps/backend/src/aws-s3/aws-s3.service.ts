d0cc6298fe130414115d55960403b9796923c657
Rayyan Mridha <rayyanmridha@gmail.com>
2025-10-01 07:03:42 -0400

    add initial aws module

---
+++ apps/backend/src/aws-s3/aws-s3.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import * as dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '../../../.env') });
+
+@Injectable()
+export class AWSS3Service {
+  private client: S3Client;
+  private readonly bucketName = process.env.AWS_BUCKET_NAME;
+  private readonly region: string;
+
+  constructor() {
+    this.region = process.env.AWS_REGION || 'us-east-2';
+    this.bucketName = process.env.AWS_BUCKET_NAME;
+
+    if (!this.bucketName) {
+      throw new Error(
+        'AWS_BUCKET_NAME is not defined in environment variables',
+      );
+    }
+
+    this.client = new S3Client({
+      region: this.region,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+  }
+
+  // In the form of "JohnDoe-Resume.pdf"
+  createLink(person: string, type: string): string {
+    const fileName = `${person}-${type}.pdf`;
+    return `https://${this.bucketName}.s3.us-east-2.amazonaws.com/${fileName}`;
+  }
+
+  async upload(
+    fileBuffer: Buffer,
+    fileName: string,
+    mimeType: string,
+  ): Promise<string> {
+    try {
+      const command = new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: fileName,
+        Body: fileBuffer,
+        ContentType: mimeType,
+      });
+
+      await this.client.send(command);
+
+      return `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${fileName}`;
+    } catch (error) {
+      throw new Error('File upload to AWS failed: ' + error);
+    }
+  }
+}
