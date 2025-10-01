d990085836195e1ca771810e19e589b663440d31
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2025-10-01 10:33:02 -0400

    added admin controller, entity, module, and service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import AppDataSource from './data-source';
+import { AdminsModule } from './users/admins.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options), AdminsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/users/types.ts
+++ apps/backend/src/users/types.ts
@@ -2,3 +2,7 @@ export enum Status {
   ADMIN = 'ADMIN',
   STANDARD = 'STANDARD',
 }
+
+export enum Site {
+  Fenway,
+}
