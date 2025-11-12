0f4b79e5caba7b84b6536cc21ab69b9dd0c2dd81
chnnick <nickchenschool@gmail.com>
2025-11-12 17:35:55 -0500

    total applications card

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -1,5 +1,18 @@
+import { TotalApplicationsCard } from '@components/TotalApplicationsCard';
+import { UsersRound } from 'lucide-react';
+
 const Root: React.FC = () => {
-  return <>Welcome to scaffolding!</>;
+  return (
+    <>
+      Welcome to scaffolding!
+      <TotalApplicationsCard
+        title="Total Applications"
+        count={298}
+        description="All time submissions"
+        icon={<UsersRound />}
+      />
+    </>
+  );
 };
 
 export default Root;
